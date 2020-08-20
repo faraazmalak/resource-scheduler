2fc0e7efb337381f78cc7a24e41ab6ce20798884
Faraaz Malak <faraazmalak@gmail.com>
2020-08-20 20:38:23 +0530

    Committing build files

---
+++ build/precache-manifest.85f5a6c81c21dee3787cc74fd13158ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c965233ff67db25b1e08ae786f622975",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23602f4ffdc67f9efd4f",
+    "url": "./static/css/2.c4b42293.chunk.css"
+  },
+  {
+    "revision": "6ded86a9984c65287a11",
+    "url": "./static/css/main.d383f157.chunk.css"
+  },
+  {
+    "revision": "23602f4ffdc67f9efd4f",
+    "url": "./static/js/2.e02b06a2.chunk.js"
+  },
+  {
+    "revision": "963b40d6695320a7e37c891bdb09daca",
+    "url": "./static/js/2.e02b06a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ded86a9984c65287a11",
+    "url": "./static/js/main.48272a45.chunk.js"
+  },
+  {
+    "revision": "eb7c46a87435d366ad6c",
+    "url": "./static/js/runtime-main.944c68d2.js"
+  }
+]);
